5f9a9d4829b98cd7b191c536c918fbb4bfe140d1
katherinekhine <khaingkatherine@gmail.com>
2024-08-24 12:41:31 +0630

    add Database.php

---
+++ classes/Database.php
@@ -0,0 +1,35 @@
+<?php
+
+class Database
+{
+    private $dbhost;
+    private $dbname;
+    private $dbusername;
+    private $dbpassword;
+    private $conn;
+
+    public function __construct($dbhost = "localhost", $dbname = "bookstore", $dbusername = "root", $dbpassword = "")
+    {
+        $this->dbhost = $dbhost;
+        $this->dbname = $dbname;
+        $this->dbusername = $dbusername;
+        $this->dbpassword = $dbpassword;
+    }
+
+    public function connect()
+    {
+        try {
+            $this->conn = new PDO("mysql:host=$this->dbhost;dbname=$this->dbname", $this->dbusername, $this->dbpassword);
+            return $this->conn;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+}
+
+
+// for testing
+$db = new Database();
+print_r($db);
+echo "<br>";
+print_r($db->connect());
