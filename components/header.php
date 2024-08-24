d46859179a42dd98ccb0208fd9fac6e5aba45094
katherinekhine <khaingkatherine@gmail.com>
2024-08-24 11:22:12 +0630

    first commit

---
+++ components/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookStore</title>
+    <!-- tailwind css -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- material link -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
+
+<body>
+    <nav class="flex justify-between item-center p-4 bg-green-700">
+        <a href="" class="text-white">
+            <span class="material-symbols-outlined">
+                auto_stories
+            </span> BookStore
+        </a>
+        <ul>
+            <li class="inline-block px-4 border border-white rounded-lg mx-2">
+                <a href="index.php" class="text-white">
+                    <span class="material-symbols-outlined">
+                        home
+                    </span> Home
+                </a>
+            </li>
+            <li class="inline-block px-4 border border-white rounded-lg mx-2">
+                <a href="#" class="text-white">
+                    <span class="material-symbols-outlined">
+                        add_circle
+                    </span> New Book
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <div class="container mx-auto">
